8113c064ffaa48558263a9a38aaaca182de24c07
reomorisawa <kuropon.africa@gmail.com>
2017-06-10 13:54:07 +0000

    footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+	$("footer-button").click(function(){
+		$("footer-button").css('background-color', 'red');
+	});
+});
